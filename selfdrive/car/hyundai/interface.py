2ae6aa5964ee356322ba1f6dd4e2e2ca94c46f62
HKGMC <47934805+HKGMC@users.noreply.github.com>
2020-02-09 19:38:36 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 Wj
 from cereal import car
 from selfdrive.config import Conversions as CV
 from selfdrive.controls.lib.drive_helpers import EventTypes as ET, create_event
